1329993ab6972cf1237c6c3c48abd848d9fc1fee
JYOTI MEHRA <mehrajyoti1209@gmail.com>
2025-03-01 23:11:19 +0530

    Create nodehealth.sh

---
+++ nodehealth.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+###########################################################
+#
+# Author:- Jyoti Mehra
+# Date:- 01/03/2025
+# 
+# This script provides output for nodehealth
+#
+# Version :- V1
+###########################################################
+set -x #debug mode
+
+free-g
+
+df -hP
+
+nproc
