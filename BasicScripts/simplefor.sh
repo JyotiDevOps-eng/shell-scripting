a9ad472f97dbcd3cd2f8698d054793bb35f1c2c7
JYOTI MEHRA <mehrajyoti1209@gmail.com>
2025-03-02 22:15:12 +0530

    modify for more readable output

--- BasicScripts/simplefor.sh
+++ BasicScripts/simplefor.sh
@@ -11,18 +11,21 @@
 ###########################################################
 #Lets print numbers 0 to 10
 
+echo "List numbers 0 to 10"
 for i in {0..10};   #by default for loop increments by 1
         do echo $i;
 done
 
 #lets print numbers 0 to 10 with increment of 3
 
+echo "List 0 to 10 in increasing order by 3"
 for i in {0..10..3};   #increments by 3
         do echo $i;
 done
 
-#lets print numbers 10 to 1 with decrement of 2
+#lets print numbers 10 to 1with decrement of 2
 
+echo "List 10 to 0 in decreasing order by 2"
 for i in {10..1..2};   #decrements by 2
         do echo $i;
 done
