fb9181ffb3755e659f42082dd1d60175983c4314
JYOTI MEHRA <mehrajyoti1209@gmail.com>
2025-03-02 15:29:11 +0530

    Create arithmectic_operations.sh

---
+++ BasicScripts/arithmectic_operations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+######################################################################
+#
+# Author:- Jyoti Mehra
+# Date:- 02/03/2025
+#
+# This script is written to perform basic arithmetic operations
+#
+# Version :- V1
+#######################################################################
+
+a=4
+b=2
+
+echo "Addition of two numbers is" $(($a+$b))
+echo "Substraction of two numbers is" $(($a-$b))
+echo "Multiplication of two numbers is" $(($a*$b))
+echo "Division of two numbers is" $(($a/$b))
